b6a0604da1481d2c889d05ad170ad29ad1f4f6ae
Viluredfish <bouscaratnicolas@gmail.com>
2015-01-06 13:26:48 +0100

    relocate render template

--- js/main.js
+++ js/main.js
@@ -22,14 +22,14 @@ var app = {
 
     initialize: function() {
         var self = this;
-        
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
             self.renderHomeView();
             
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
     }
 
 };
